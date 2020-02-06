583ddf276df1cf7dfb8cc0dd1c353576c124675c
Rakesh <sairakesh217@gmail.com>
2020-02-06 13:07:21 -0500

    Comiting work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Header from './Header.js';
 
-function App() {
-  return (
+class App extends Component {
+  render(){
+    return (
     <div>
       <Header />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { render } from '@testing-library/react';
 import App from './App';
+import Header from './Header.js';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testing Login', () => {
+  let wrapper;
+  test('username check', () => {
+    wrapper = shallow(<Header />);
+    wrapper.find('input[type="email"]').simulate('change', {
+      target: {name: 'username', value: 'rakesh@avi'}
+    });
+    expect(wrapper.state('username')).toEqual('rakesh@avi');
+  })
+  it('password check', () => {
+    wrapper = shallow(<Header />);
+    wrapper.find('input[type="password"]').simulate('change', {
+      target: {
+        name: "password", value: "rakesh123"
+      }
+    });
+    expect(wrapper.state('password')).toEqual('rakesh123');
+  })
+})
